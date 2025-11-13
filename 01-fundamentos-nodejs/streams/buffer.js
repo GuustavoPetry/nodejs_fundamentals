1909288317b40f714cda0f77c1d23c8a028c0f90
GuustavoPetry <ogustavopetry@gmail.com>
2025-11-13 09:07:12 -0300

    Definição de Buffer

---
+++ 01-fundamentos-nodejs/streams/buffer.js
@@ -0,0 +1,7 @@
+// Buffer é uma representação de espaço na memória do computador
+// Utilizado para transitar dados de forma ágil em hexadecimal
+
+// Convertendo String em Buffer
+const buf = Buffer.from("Isso é um Buffer");
+// Retorna em Hexadecimal
+console.log(buf);
